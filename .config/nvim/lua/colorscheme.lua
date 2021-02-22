ce880b7d0049b0486d8b579d2d1d500b2dedceec
Maximilian Siling <mouse-art@ya.ru>
2021-02-22 22:28:20 +0300

    Remove duplication from colorscheme.lua

--- .config/nvim/lua/colorscheme.lua
+++ .config/nvim/lua/colorscheme.lua
@@ -7,14 +7,3 @@ else
 	-- ..but `atom-dark` sets it for us
 	vim.cmd [[ colorscheme atom-dark ]]
 end
-
--- Set colorscheme
-if vim.g.notepad_mode then
-	-- We have to set `background=light` for `one` manually...
-	setopt('o', 'background', 'light')
-	vim.cmd [[ colorscheme one ]]
-else
-	-- ..but `atom-dark` sets it for us
-	vim.cmd [[ colorscheme atom-dark ]]
-end
-
