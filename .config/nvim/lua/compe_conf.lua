ff125a2cf062cea02b76bf76b3c5b1eb119a87da
Maximilian Siling <mouse-art@ya.ru>
2021-03-18 10:28:18 +0300

    Fix completion

--- .config/nvim/lua/compe_conf.lua
+++ .config/nvim/lua/compe_conf.lua
@@ -12,19 +12,21 @@ require('compe').setup {
 	};
 }
 
-vimp.inoremap({'silent', 'expr'}, '<Tab>', function()
+function _G.tab_complete()
 	if vim.fn.pumvisible() ~= 0 then
-		return '<C-n>'
+		-- ^N
+		return vim.fn.nr2char(14)
 	end
 
 	local line = vim.fn.getline('.')
 	line = string.sub(line, 1, vim.fn.col('.') - 1)
 	if string == nil or string.match(line, "%S") == nil then
-		return '<Tab>'
+		return '	'
 	end
 	return vim.fn['compe#complete']()
-end)
+end
 
+vimp.inoremap({'silent', 'expr'}, '<Tab>', 'v:lua.tab_complete()')
 vimp.inoremap({'silent'}, '<S-Tab>', '<C-p>')
 vimp.inoremap({'silent', 'expr'}, '<CR>', "compe#confirm('<CR>')")
 vimp.inoremap({'silent', 'expr'}, '<C-e>', "compe#close('<C-e>')")
