2a29f116939a94103ed15b2a9b1ed777c5d265bb
Goldstein <root@goldstein.rs>
2022-09-26 17:54:37 +0300

    more null-ls config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -129,8 +129,22 @@ use {
 		local null_ls = require 'null-ls'
 		null_ls.setup {
 			sources = {
-				null_ls.builtins.diagnostics.shellcheck,
-				null_ls.builtins.code_actions.shellcheck,
+				null_ls.builtins.diagnostics.shellcheck.with {
+					condition = function()
+						return vim.fn.executable 'shellcheck' == 1
+					end
+				},
+				null_ls.builtins.code_actions.shellcheck.with {
+					condition = function()
+						return vim.fn.executable 'shellcheck' == 1
+					end
+				},
+				null_ls.builtins.formatting.jq.with {
+					condition = function()
+						return vim.fn.executable 'jq' == 1
+					end,
+
+				},
 			}
 		}
 	end
